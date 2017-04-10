e56cd946e905f2889c07a8f78f734bf27a5150ff
moevegas <moevegas@gmail.com>
2017-04-10 00:38:07 -0400

    API for Libary Connecting

---
+++ NetLibrary/Controllers/LibraryController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NetLibrary.Models;
+
+namespace NetLibrary.Controllers
+{
+    public class LibraryController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetAllBooks()
+        {
+            // this is where the code (sqlCommand)
+
+            //return Ok("Oh yeah, I think it works");
+
+            // DatabaseLocation
+            const string connectionString =
+                @"Server=localhost\SQLEXPRESS;Database=MyLibraryDB;Trusted_Connection=True;";
+
+
+            var bookList = new List<BookClass>();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("Connected!");
+                var sqlCommand = new SqlCommand();
+                sqlCommand.Connection = connection;
+                sqlCommand.CommandType = System.Data.CommandType.Text;
+                sqlCommand.CommandText = @"SELECT * FROM LibraryTable";
+                connection.Open();
+
+
+                var reader = sqlCommand.ExecuteReader();
+
+
+                while (reader.Read())
+                {
+                    var bookz = new BookClass(reader);
+                    bookList.Add(bookz);
+                }
+
+
+
+                connection.Close();
+            }
+
+            return Ok(bookList);
+
+        }
+        [HttpPut]
+        public IHttpActionResult AddBook ()
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ NetLibrary/Models/BookClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace NetLibrary.Models
+{
+    public class BookClass
+    {
+       
+
+        public BookClass(SqlDataReader reader)
+        {
+            this.Title = reader["Title"].ToString();
+            this.Author = reader["Author"].ToString();
+            this.YearPublished = (int)reader["YearPublished"];
+            this.Genre = reader["Genre"].ToString();
+            this.IsCheckedOut = (bool)reader["IsCheckedOut"];
+            this.LastCheckedOut = reader["LastCheckedOut"].ToString();
+            this.DueBackDate = reader["DueBackDate"].ToString();
+
+        }
+
+        public string id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int YearPublished { get; set; }
+        public string Genre { get; set; }
+        public bool IsCheckedOut { get; set; }
+        public string LastCheckedOut { get; set; }
+        public string DueBackDate { get; set; }
+
+    }
+}
